932944d5bca5ed1bba0215393c209ddc0983d2c1
Dustin Wurtz <dtwurtz@gmail.com>
2016-04-11 10:56:54 -0700

    Changed authorization access for users

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:id/regularStuff', (req, res, next) => {
+router.use((req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+});
+
+function myAccountRequired(req, res, next) {
+  if (req.session.user.id.toString() === req.params.id) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+
+router.get('/:id/regularStuff', myAccountRequired, (req, res, next) => {
   res.render('users/regularStuff')
 });
 
-router.get('/:id/specialStuff', (req, res, next) => {
+router.get('/:id/specialStuff', myAccountRequired, (req, res, next) => {
   res.render('users/specialStuff')
 });
 
